1ace3d09dfbe097be7e41756aff4e50dda540076
Jed <jedaka@jedaka.local>
2021-12-03 17:27:36 +0300

    Change staking title

--- src/views/Stake/index.tsx
+++ src/views/Stake/index.tsx
@@ -103,7 +103,7 @@ function Stake() {
                     <Grid className="stake-card-grid" container direction="column" spacing={2}>
                         <Grid item>
                             <div className="stake-card-header">
-                                <p className="stake-card-header-title">QUAS Staking (🎩, 🎩)</p>
+                                <p className="stake-card-header-title">Staking (3,3)</p>
                                 <RebaseTimer />
                             </div>
                         </Grid>
