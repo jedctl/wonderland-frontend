670afb7476aa2cb84296f33d39b3d7cb00cea268
Jed <jedaka@jedaka.local>
2021-12-03 16:37:49 +0300

    Delete zapin

--- src/views/Bond/BondPurchase.tsx
+++ src/views/Bond/BondPurchase.tsx
@@ -11,7 +11,6 @@ import { IAllBondData } from "../../hooks/bonds";
 import useDebounce from "../../hooks/debounce";
 import { messages } from "../../constants/messages";
 import { warning } from "../../store/slices/messages-slice";
-import Zapin from "./Zapin";
 
 interface IBondPurchaseProps {
     bond: IAllBondData;
@@ -27,7 +26,6 @@ function BondPurchase({ bond, slippage, recipientAddress }: IBondPurchaseProps) 
     const [useAvax, setUseAvax] = useState(false);
 
     const isBondLoading = useSelector<IReduxState, boolean>(state => state.bonding.loading ?? true);
-    const [zapinOpen, setZapinOpen] = useState(false);
 
     const pendingTransactions = useSelector<IReduxState, IPendingTxn[]>(state => {
         return state.pendingTransactions;
@@ -109,16 +107,6 @@ function BondPurchase({ bond, slippage, recipientAddress }: IBondPurchaseProps) 
         if (await checkWrongNetwork()) return;
 
         dispatch(changeApproval({ address, bond, provider, networkID: chainID }));
-    };
-
-    const handleZapinOpen = () => {
-        dispatch(calcBondDetails({ bond, value: "0", provider, networkID: chainID }));
-        setZapinOpen(true);
-    };
-
-    const handleZapinClose = () => {
-        dispatch(calcBondDetails({ bond, value: quantity, provider, networkID: chainID }));
-        setZapinOpen(false);
     };
 
     const displayUnits = useAvax ? "AVAX" : bond.displayUnits;
@@ -230,7 +218,6 @@ function BondPurchase({ bond, slippage, recipientAddress }: IBondPurchaseProps) 
                     )}
                 </Box>
             </Slide>
-            {/* <Zapin open={zapinOpen} handleClose={handleZapinClose} bond={bond} /> */}
         </Box>
     );
 }
