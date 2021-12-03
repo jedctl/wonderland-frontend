0885fda7bb543e82e0b1244e508608f9ce041405
Jed <jedaka@jedaka.local>
2021-12-03 15:47:07 +0300

    Delete landing

--- src/Root/index.tsx
+++ src/Root/index.tsx
@@ -1,15 +1,10 @@
 import React, { useEffect, useState } from "react";
 import App from "./App";
-import Landing from "./Landing";
 import { HashRouter } from "react-router-dom";
 import { loadTokenPrices } from "../helpers";
 import Loading from "../components/Loader";
 
 function Root() {
-    const isApp = (): boolean => {
-        return window.location.host.includes("app");
-    };
-
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
